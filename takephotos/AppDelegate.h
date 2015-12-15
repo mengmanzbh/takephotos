77c586c576e9243dd41ddcbf56ae294a61215e65
mengmanzbh <1934068305@qq.com>
2015-12-15 09:37:16 +0800

    Initial Commit

---
+++ takephotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  takephotos
+//
+//  Created by kuailefu on 15/12/15.
+//  Copyright © 2015年 kuailefu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
